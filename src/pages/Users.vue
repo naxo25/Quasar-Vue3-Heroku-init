0b22165e4a14c2c9292b3260e83582eec4700a59
naxo25 <y.labra.25@gmail.com>
2021-08-14 16:21:01 -0400

    example users from api && components bind user

---
+++ src/pages/Users.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <Card
+      v-for="user in users"
+      :key="user"
+      v-bind="user"
+      :user='user'
+    />
+  </div>
+</template>
+
+<script>
+
+import Card from '../components/card.vue'
+import { ref } from 'vue'
+
+export default ({
+  name: 'MainLayout',
+
+  components: {
+    Card
+  },
+
+  setup () {
+    const users = ref({})
+
+    fetch('https://randomuser.me/api/?results=5').then(async response => {
+      const data = await response.json()
+      users.value = data.results
+    })
+
+    return {
+			users
+    }
+  }
+})
+</script>
